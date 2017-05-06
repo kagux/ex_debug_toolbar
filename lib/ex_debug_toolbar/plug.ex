03900e78ea6414e0456f6de21a4ebc00ffb29f16
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-06 22:16:26 +0200

    grouped inject? logic in one place

--- lib/ex_debug_toolbar/plug.ex
+++ lib/ex_debug_toolbar/plug.ex
@@ -11,13 +11,11 @@ defmodule ExDebugToolbar.Plug do
     register_before_send conn, &inject_debug_toolbar_code/1
   end
 
-  defp inject_debug_toolbar_code(%Conn{status: 200} = conn) do
-    conn |> inject? |> append_code_to_resp_body(conn)
+  defp inject_debug_toolbar_code(conn) do
+    if inject?(conn), do: append_code_to_resp_body(conn), else: conn
   end
-  defp inject_debug_toolbar_code(conn), do: conn
 
-  defp append_code_to_resp_body(false, conn), do: conn
-  defp append_code_to_resp_body(true, conn) do
+  defp append_code_to_resp_body(conn) do
     resp_body = to_string(conn.resp_body)
     body = String.replace(resp_body, "</body>", debug_toolbar_tag(conn) <> "</body>")
     put_in conn.resp_body, body
@@ -27,11 +25,13 @@ defmodule ExDebugToolbar.Plug do
     "TOOLBAR"
   end
 
-  defp inject?(conn) do
+  defp inject?(%Conn{status: 200} = conn) do
     conn
     |> get_resp_header("content-type")
     |> html_content_type?
   end
+  defp inject?(_), do: false
+
   defp html_content_type?([]), do: false
   defp html_content_type?([type | _]), do: String.starts_with?(type, "text/html")
 end
