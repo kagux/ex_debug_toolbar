e387986652000b7e0f316e1d5912b9ef8c76c284
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-08 19:05:50 +0200

    added mock/meck library

--- mix.exs
+++ mix.exs
@@ -28,11 +28,14 @@ defmodule ExDebugToolbar.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:mock, "~> 0.2", only: :test},
+    ]
   end
 end
