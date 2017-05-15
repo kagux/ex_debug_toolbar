7e7ea664280de33562586887df048a7bdfcaffec
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-16 01:01:47 +0200

    dummy log entry encoder

--- lib/ex_debug_toolbar/data/log_entry.ex
+++ lib/ex_debug_toolbar/data/log_entry.ex
@@ -12,3 +12,15 @@ alias ExDebugToolbar.Data.{Collectable, LogEntry}
 defimpl Collectable, for: LogEntry do
   def init_collection(_), do: []
 end
+
+defimpl Poison.Encoder, for: LogEntry do
+  def encode(entry, _options) do
+    %{
+      level: entry.level,
+      message: inspect(entry.message),
+      metadata: inspect(entry.metadata),
+      timestamp: inspect(entry.timestamp)
+    }
+    |> Poison.encode!
+  end
+end
