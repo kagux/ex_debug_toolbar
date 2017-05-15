960dd41dce061c34358ff984235280d3bf5494fb
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-15 08:28:46 +0200

    typo

--- test/lib/ex_debug_toolbar/data/event_test.exs
+++ test/lib/ex_debug_toolbar/data/event_test.exs
@@ -2,7 +2,7 @@ defmodule ExDebugToolbar.Data.EventTest do
   use ExUnit.Case, async: true
   alias ExDebugToolbar.Data.{Collectable, Event, Timeline}
 
-  test "implements collecable protocol" do
+  test "implements collectable protocol" do
     event = %Event{name: "test"}
     assert Collectable.init_container(event) == %Timeline{}
     assert %Timeline{events: [%Event{name: "test"}]} = Collectable.put(event, %Timeline{})
