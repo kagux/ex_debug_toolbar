4a453f51474b8f3812506d3b857577223976a1e3
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-17 07:28:33 +0200

    typo

--- test/lib/ex_debug_toolbar/data/log_entry_test.exs
+++ test/lib/ex_debug_toolbar/data/log_entry_test.exs
@@ -2,7 +2,7 @@ defmodule ExDebugToolbar.Data.LogEntryTest do
   use ExUnit.Case, async: true
   alias ExDebugToolbar.Data.{Collectable, LogEntry}
 
-  test "implements collecable protocol" do
+  test "implements collectable protocol" do
     assert Collectable.init_collection(%LogEntry{}) == []
   end
 end
