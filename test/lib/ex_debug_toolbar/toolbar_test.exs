54e6b59190d5b48e0c634f05191adce8f6887ef6
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-22 14:39:28 +0200

    use collector case in toolbar test

--- test/lib/ex_debug_toolbar/toolbar_test.exs
+++ test/lib/ex_debug_toolbar/toolbar_test.exs
@@ -1,22 +1,19 @@
 defmodule ExDebugToolbar.ToolbarTest do
-  use ExUnit.Case, async: true
+  use ExDebugToolbar.CollectorCase, async: false
   alias ExDebugToolbar.Toolbar
-  import ExDebugToolbar.Test.Support.RequestHelpers
 
   describe "add_data/3" do
+    setup :start_request
+
     test "it sets new data" do
-      Process.put(:request_id, "request_id_1")
-      Toolbar.start_request
-      Toolbar.add_data("request_id_1", :foo, %{foo: :bar})
+      Toolbar.add_data(@request_id, :foo, %{foo: :bar})
       {:ok, request} = get_request()
       assert request.data.foo == %{foo: :bar}
     end
 
     test "it updates existing data" do
-      Process.put(:request_id, "request_id_2")
-      Toolbar.start_request
-      Toolbar.add_data("request_id_2", :foo, %{foo: :bar})
-      Toolbar.add_data("request_id_2", :foo, %{faz: :baz})
+      Toolbar.add_data(@request_id, :foo, %{foo: :bar})
+      Toolbar.add_data(@request_id, :foo, %{faz: :baz})
       {:ok, request} = get_request()
       assert request.data.foo == %{foo: :bar, faz: :baz}
     end
