a6a49e20830e0c68115157b2a69924b08bedb0c0
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-24 15:39:16 +0200

    toolbar appication does nothing and starts no processes if disabled

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,19 @@
 use Mix.Config
 
-if Mix.env == :test do
-  config :ex_debug_toolbar, ExDebugToolbar.Fixtures.Endpoint,
-    instrumenters: [ExDebugToolbar.Collector.InstrumentationCollector]
+config :ex_debug_toolbar,
+  enable: true
 
-  config :ex_debug_toolbar, ExDebugToolbar.Endpoint,
-    url: [host: "localhost"],
-    secret_key_base: "v6SG14aYQCvYyk4rRq4HYYJ1GGXUIf23oWS5kmy0MngyWPTrlQAGnl1mvKkGy/Tj",
-    render_errors: [view: ExDebugToolbar.ErrorView, accepts: ~w(html json)],
-    pubsub: [name: ExDebugToolbar.PubSub,
-             adapter: Phoenix.PubSub.PG2]
+config :ex_debug_toolbar, ExDebugToolbar.Fixtures.Endpoint,
+  instrumenters: [ExDebugToolbar.Collector.InstrumentationCollector]
 
-  config :logger, :console,
-    format: "$time $metadata[$level] $message\n",
-    metadata: [:request_id],
-    level: :warn
-end
+config :ex_debug_toolbar, ExDebugToolbar.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "v6SG14aYQCvYyk4rRq4HYYJ1GGXUIf23oWS5kmy0MngyWPTrlQAGnl1mvKkGy/Tj",
+  render_errors: [view: ExDebugToolbar.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: ExDebugToolbar.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id],
+  level: :warn
--- lib/ex_debug_toolbar.ex
+++ lib/ex_debug_toolbar.ex
@@ -4,8 +4,12 @@ defmodule ExDebugToolbar do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
+    Application.get_env(:ex_debug_toolbar, :enable, false) |> do_start()
+  end
 
+  defp do_start(false), do: {:ok, self()}
+  defp do_start(true) do
+    import Supervisor.Spec
     # Define workers and child supervisors to be supervised
     children = [
       # Start the endpoint when the application starts
