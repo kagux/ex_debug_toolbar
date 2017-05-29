79cc487eaf6a3ccff4e58a6f44dbadff8f763563
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-29 22:14:07 +0200

    use Code.eval_quote to simplify further template collector test

--- test/lib/ex_debug_toolbar/collector/template_collector_test.exs
+++ test/lib/ex_debug_toolbar/collector/template_collector_test.exs
@@ -1,35 +1,30 @@
-
-defmodule ExDebugToolbar.Test.TemplateHelper do
-  alias ExDebugToolbar.Test.TemplateHelper.EExEngine
-
-  defmodule EExEngine do
-    use ExDebugToolbar.Collector.TemplateCollector, engine: Phoenix.Template.EExEngine
-  end
-
-  defmacro compile_template(name) do
-    path = "test/fixtures/#{name}.eex"
-    EExEngine.compile(path, name)
-  end
+defmodule ExDebugToolbar.Test.EExEngine do
+  use ExDebugToolbar.Collector.TemplateCollector, engine: Phoenix.Template.EExEngine
 end
 
 defmodule ExDebugToolbar.Collector.TemplateTest do
   use ExDebugToolbar.CollectorCase, async: false
-  require ExDebugToolbar.Test.TemplateHelper
-  alias ExDebugToolbar.Test.TemplateHelper
+  alias ExDebugToolbar.Test.EExEngine
   alias ExDebugToolbar.Data.Timeline.Event
 
   setup :start_request
 
   test "it compiles template file using provided engine" do
-    compiled_template = TemplateHelper.compile_template("template.html")
-    assert compiled_template == {:safe, ["" | "<div> Hello world! </div>\n"]}
+    compiled_template = compile_template("template.html")
+    assert compiled_template == {{:safe, ["" | "<div> Hello world! </div>\n"]}, []}
   end
 
   test "it tracks render time" do
-    TemplateHelper.compile_template("template.html")
+    compile_template("template.html")
     assert {:ok, request} = get_request()
     timeline = request.data.timeline
     assert timeline.duration > 0
     assert %Event{name: "template#test/fixtures/template.html.eex"} = timeline.events |> hd
+  end
+
+  def compile_template(name) do
+    "test/fixtures/#{name}.eex"
+    |> EExEngine.compile(name)
+    |> Code.eval_quoted
   end
 end
