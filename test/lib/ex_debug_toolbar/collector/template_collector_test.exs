2420b6395f1cc905761770a788ff5cdf1725e0b8
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-29 22:06:06 +0200

    improved template tests to assert against final html rather then macro contents

--- test/lib/ex_debug_toolbar/collector/template_collector_test.exs
+++ test/lib/ex_debug_toolbar/collector/template_collector_test.exs
@@ -1,26 +1,35 @@
-defmodule ExDebugToolbar.Test.EExEngine do
-  use ExDebugToolbar.Collector.TemplateCollector, engine: Phoenix.Template.EExEngine
+
+defmodule ExDebugToolbar.Test.TemplateHelper do
+  alias ExDebugToolbar.Test.TemplateHelper.EExEngine
+
+  defmodule EExEngine do
+    use ExDebugToolbar.Collector.TemplateCollector, engine: Phoenix.Template.EExEngine
+  end
+
+  defmacro compile_template(name) do
+    path = "test/fixtures/#{name}.eex"
+    EExEngine.compile(path, name)
+  end
 end
 
 defmodule ExDebugToolbar.Collector.TemplateTest do
-  use ExUnit.Case, async: true
-  alias ExDebugToolbar.Test.EExEngine
+  use ExDebugToolbar.CollectorCase, async: false
+  require ExDebugToolbar.Test.TemplateHelper
+  alias ExDebugToolbar.Test.TemplateHelper
+  alias ExDebugToolbar.Data.Timeline.Event
+
+  setup :start_request
 
   test "it compiles template file using provided engine" do
-    {_, _, [_, template]} = compile_template()
-    code = template |> Macro.to_string
-    assert code == "fn -> {:safe, [\"\" | \"<div> Hello world! </div>\\n\"]} end"
+    compiled_template = TemplateHelper.compile_template("template.html")
+    assert compiled_template == {:safe, ["" | "<div> Hello world! </div>\n"]}
   end
 
   test "it tracks render time" do
-    compile_template()
-    {function, _, [event_name, _]} = compile_template()
-    assert function |> Macro.to_string == "Toolbar . :record_event"
-    assert event_name |> Macro.to_string == "\"template#\#{\"test/fixtures/template.html.eex\"}\""
-  end
-
-  defp compile_template do
-    path = "test/fixtures/template.html.eex"
-    EExEngine.compile(path, "test.html")
+    TemplateHelper.compile_template("template.html")
+    assert {:ok, request} = get_request()
+    timeline = request.data.timeline
+    assert timeline.duration > 0
+    assert %Event{name: "template#test/fixtures/template.html.eex"} = timeline.events |> hd
   end
 end
