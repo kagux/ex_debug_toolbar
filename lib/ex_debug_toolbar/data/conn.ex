2b908b63e893eab276aee74fd724c890b2aebe76
Boris Mikhaylov <kaguxmail@gmail.com>
2017-05-29 22:19:27 +0200

    removed keys duplication in conn data

--- lib/ex_debug_toolbar/data/conn.ex
+++ lib/ex_debug_toolbar/data/conn.ex
@@ -1,33 +1,31 @@
 defmodule ExDebugToolbar.Data.Conn do
-  defstruct ~w(
-    host method script_name request_path port remote_ip scheme
-    query_string body_params query_params path_params req_cookies req_headers
-    status resp_charset assigns resp_cookies resp_headers
-  )a
-end
-alias ExDebugToolbar.Data.{Collection, Conn}
-
-defimpl Collection, for: Conn do
-
   @request_keys ~w(
     host method script_name request_path port remote_ip scheme
     query_string body_params query_params path_params req_cookies req_headers
   )a
-
   @response_keys ~w(status resp_charset assigns resp_cookies resp_headers)a
 
+  defstruct @request_keys ++ @response_keys
+
+  def request_keys, do: @request_keys
+  def response_keys, do: @response_keys
+end
+
+alias ExDebugToolbar.Data.{Collection, Conn}
+
+defimpl Collection, for: Conn do
   def add(collection, changes) when is_map(changes) do
     Map.merge(collection, changes)
   end
 
   def format_item(_collection, {:request, %Plug.Conn{} = conn}) do
     conn
-    |> Map.take(@request_keys)
+    |> Map.take(Conn.request_keys())
     |> Map.update!(:remote_ip, &format_ip/1)
   end
 
   def format_item(_collection, {:response, %Plug.Conn{} = conn}) do
-    conn |> Map.take(@response_keys)
+    conn |> Map.take(Conn.response_keys())
   end
 
   defp format_ip(nil), do: nil
