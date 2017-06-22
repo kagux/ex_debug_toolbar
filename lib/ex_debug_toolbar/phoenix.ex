c386bad003bd875ff1d4118867c48eb3c49fd70e
Boris Mikhaylov <kaguxmail@gmail.com>
2017-06-22 15:43:40 +0200

    use enable flag inside __use__ macro to avoid compilation time hardcoded config

--- lib/ex_debug_toolbar/phoenix.ex
+++ lib/ex_debug_toolbar/phoenix.ex
@@ -1,36 +1,34 @@
-if Application.get_env(:ex_debug_toolbar, :enable, false) do
-  defmodule ExDebugToolbar.Phoenix do
-    defmacro __using__(_) do
-      quote do
-        require Phoenix.Endpoint
-        alias Phoenix.Endpoint
-        alias ExDebugToolbar.Plug.Router
+defmodule ExDebugToolbar.Phoenix do
+  defmacro __using__(_) do
+    if Application.get_env(:ex_debug_toolbar, :enable), do: build_plug_ast()
+  end
 
-        Logger.add_backend(ExDebugToolbar.Collector.LoggerCollector)
+  def build_plug_ast do
+    quote do
+      require Phoenix.Endpoint
+      alias Phoenix.Endpoint
+      alias ExDebugToolbar.Plug.Router
 
-        Endpoint.socket "/__ex_debug_toolbar__/socket", ExDebugToolbar.UserSocket
+      Logger.add_backend(ExDebugToolbar.Collector.LoggerCollector)
 
-        def call(conn, opts) do
-          case dispatch_router(conn, opts) do
-            %{private: %{phoenix_endpoint: ExDebugToolbar.Endpoint}} = conn ->
-              conn
-            conn ->
-            Endpoint.instrument(conn, :ex_debug_toolbar, %{conn: conn}, fn ->
-              super(conn, opts)
-            end)
-          end
-        end
-        defoverridable [call: 2]
+      Endpoint.socket "/__ex_debug_toolbar__/socket", ExDebugToolbar.UserSocket
 
-        defp dispatch_router(conn, opts) do
-          opts = Router.init(opts)
-          Router.call(conn, opts)
+      def call(conn, opts) do
+        case dispatch_router(conn, opts) do
+          %{private: %{phoenix_endpoint: ExDebugToolbar.Endpoint}} = conn ->
+            conn
+          conn ->
+          Endpoint.instrument(conn, :ex_debug_toolbar, %{conn: conn}, fn ->
+            super(conn, opts)
+          end)
         end
       end
+      defoverridable [call: 2]
+
+      defp dispatch_router(conn, opts) do
+        opts = Router.init(opts)
+        Router.call(conn, opts)
+      end
     end
-  end
-else
-  defmodule ExDebugToolbar.Phoenix do
-    defmacro __using__(_), do: :ok
   end
 end
