2d9456505720a7917d650185b19e398235d888b6
Boris Mikhaylov <kaguxmail@gmail.com>
2017-09-23 21:21:17 +0200

    set conn once on response only

--- test/lib/ex_debug_toolbar/collector/conn_collector_test.exs
+++ test/lib/ex_debug_toolbar/collector/conn_collector_test.exs
@@ -1,12 +1,19 @@
 defmodule ExDebugToolbar.Collector.ConnCollectorTest do
   use ExDebugToolbar.CollectorCase, async: true
   alias ExDebugToolbar.Collector.ConnCollector, as: Collector
+  use Plug.Test
+  import Plug.Conn
 
   setup :start_request
 
-  test "it collects conn data" do
-    %Plug.Conn{request_path: "/path"} |> Collector.call(%{})
+  test "it collects conn data on response without body" do
+    conn = conn(:get, "/path")
+    |> Collector.call(%{})
+    |> send_resp(200, "body")
+
+    sent_conn = %{conn | state: :set, resp_body: nil}
+
     assert {:ok, request} = get_request()
-    assert %{request_path: "/path"} = request.conn
+    assert sent_conn == request.conn
   end
 end
