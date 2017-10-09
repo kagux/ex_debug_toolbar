1962e535d6a7d043ee9067db2048abd7a4dd301e
Boris Mikhaylov <kaguxmail@gmail.com>
2017-10-09 22:18:28 +0200

    removed unused elements from layout

--- web/views/dashboard_layout_view.ex
+++ web/views/dashboard_layout_view.ex
@@ -2,4 +2,9 @@ defmodule ExDebugToolbar.DashboardLayoutView do
   @moduledoc false
 
   use ExDebugToolbar.Web, :view
+
+  def version do
+    {:ok, version} = :application.get_key(:ex_debug_toolbar, :vsn)
+    "v#{version}"
+  end
 end
--- web/views/dashboard_view.ex
+++ web/views/dashboard_view.ex
@@ -2,4 +2,8 @@ defmodule ExDebugToolbar.DashboardView do
   @moduledoc false
 
   use ExDebugToolbar.Web, :view
+
+  def header(_), do: "Requests"
+
+  def description(_), do: "Overview of all recorded requests"
 end
