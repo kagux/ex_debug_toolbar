56a5f209431ec5bc7f056109941f59e673ee7739
Boris Mikhaylov <kaguxmail@gmail.com>
2017-11-04 16:31:13 -0400

    cleaned up unused history helpers

--- test/views/helpers/history_helpers_test.exs
+++ test/views/helpers/history_helpers_test.exs
@@ -10,15 +10,4 @@ defmodule ExDebugToolbar.View.Helpers.HistoryHelpersTest do
       assert HistoryHelpers.history_row_color(%Conn{status: nil}) == "danger"
     end
   end
-
-
-  describe "history_row_collapse_class/1" do
-    test "it is not collapsed for first row" do
-      assert HistoryHelpers.history_row_collapse_class(0) == "visible-row"
-    end
-
-    test "it is collapsed and with a group number for other rows" do
-      assert HistoryHelpers.history_row_collapse_class(1) == "invisible-row"
-    end
-  end
 end
--- web/views/helpers/history_helpers.ex
+++ web/views/helpers/history_helpers.ex
@@ -3,9 +3,6 @@ defmodule ExDebugToolbar.View.Helpers.HistoryHelpers do
 
   alias ExDebugToolbar.View.Helpers.ConnHelpers
 
-  def history_row_collapse_class(0), do: "visible-row"
-  def history_row_collapse_class(_), do: "invisible-row"
-
   def history_row_color(conn) do
     case ConnHelpers.conn_status_color_class(conn) do
       "success" -> nil
