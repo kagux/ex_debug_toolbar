54f2c458214b44f26a5a1a3f83776861ee0b4add
Boris Mikhaylov <kaguxmail@gmail.com>
2017-11-25 13:57:14 +0100

    test that toolbar channel correctly handles request:created event

--- test/channels/toolbar_channel_test.exs
+++ test/channels/toolbar_channel_test.exs
@@ -7,24 +7,30 @@ defmodule ExDebugToolbar.ToolbarChannelTest do
   describe "join" do
     test "it returns stopped request upon joining toolbar channel" do
       stop_request(@request_id)
-      {:ok, payload, _} = socket() |> join(ToolbarChannel, "toolbar:request:#{@request_id}", %{})
+      {:ok, payload, _} = subscribe_and_join_toolbar(@request_id)
       assert %{} = payload
       assert payload |> Map.has_key?(:html)
     end
 
     test "it returns pending message if request is still running" do
-      {:ok, payload, _} = socket() |> join(ToolbarChannel, "toolbar:request:#{@request_id}", %{})
+      {:ok, payload, _} = subscribe_and_join_toolbar(@request_id)
       assert :pending == payload
     end
 
     test "it returns an error if request could not be retrieved" do
-      {:error, error} = socket() |> join(ToolbarChannel, "toolbar:request:wrong_id", %{})
+      {:error, error} = subscribe_and_join_toolbar("wrong_id")
       assert %{reason: :not_found} = error
     end
   end
 
-  describe "request:created broadcast" do
+  test "pushes request to the socket on request:created event" do
+    {:ok, _, socket} = subscribe_and_join_toolbar(@request_id)
+    broadcast_from socket, "request:created", %{id: @request_id}
+    {:ok, request} = get_request(@request_id)
+    assert_push "request:created", %{html: _, request: ^request}
+  end
 
-
+  defp subscribe_and_join_toolbar(request_id) do
+    socket() |> subscribe_and_join(ToolbarChannel, "toolbar:request:#{request_id}", %{})
   end
 end
