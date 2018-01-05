76cd0bc47aef21665429d80e9699bf8c2183dc7b
Boris Mikhaylov <kaguxmail@gmail.com>
2018-01-05 14:43:58 +0100

    simple mock up of single request page

--- web/views/dashboard/request_view.ex
+++ web/views/dashboard/request_view.ex
@@ -3,7 +3,11 @@ defmodule ExDebugToolbar.Dashboard.RequestView do
 
   use ExDebugToolbar.Web, :view
 
+  def header(%{private: %{phoenix_action: :show}}), do: "Request"
   def header(_), do: "History"
 
-  def description(_), do: "Overview of all recorded requests"
+  def description(%{private: %{phoenix_action: :show}}) do 
+    "Let's dive into details"
+  end
+  def description(_), do: "Overview of recorded requests"
 end
