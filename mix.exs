5113d54278a40a58b383abfb8bb7ca6a9305cc4f
Boris Mikhaylov <kaguxmail@gmail.com>
2018-03-27 14:40:31 +0200

    use phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule ExDebugToolbar.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
